dba295d7ac7448b9f16d9edd72c0d5cbe6c2f8a4
Rohit Das <rohit.das950@gmail.com>
2017-11-12 11:21:05 +0530

    Files added, modified:dimens.xml,MainActivity.java
    dimens.xml has been added to remove dependency error occurring in
    activity_main.xml due to @dimen/activity_vertical_margin.
    
    MainActivity.java has been modified to handle messages sent by both
    user and bot. Static message of "Hello world" set up for now for
    debugging purposes. Please run and check.
    
    Also, libraries have been added in build.gradle(Module:app) for
    FloatingActionButton to work.
        compile 'com.android.support:design:26.1.0'
    
    Step 1 complete.
    
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

--- AIRYL-bot/app/src/main/java/com/phoenix/airyl/MainActivity.java
+++ AIRYL-bot/app/src/main/java/com/phoenix/airyl/MainActivity.java
@@ -1,10 +1,28 @@
 package com.phoenix.airyl;
 
+import android.content.res.AssetManager;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.phoenix.airyl.Adapter.ChatMsgAdapter;
+import com.phoenix.airyl.Pojo.ChatMsg;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private FloatingActionButton mBtnSend;
+    private EditText mEditTxtMsg;
+    private ImageView mImgView;
+    private ChatMsgAdapter mAdapter;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,14 +34,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mListView = (ListView) findViewById(R.id.listView);
+        mBtnSend = (FloatingActionButton) findViewById(R.id.btn_send);
+        mEditTxtMsg = (EditText) findViewById(R.id.et_message);
+        mImgView = (ImageView) findViewById(R.id.iv_image);
+
+        mAdapter = new ChatMsgAdapter(this, new ArrayList<ChatMsg>());
+
+        //code for sending message
+        mBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = mEditTxtMsg.getText().toString();
+                sendMsg(msg);
+                mEditTxtMsg.setText("");
+                mListView.setSelection(mAdapter.getCount() - 1);
+            }
+        });
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void sendMsg (String msg) {
+        ChatMsg chatmsg = new ChatMsg(msg, true, false);
+        mAdapter.add(chatmsg);
+        //respond as Hello world
+        mimicOtherMsg("Hello world");
+    }
+
+    private void mimicOtherMsg (String msg) {
+        ChatMsg chatmsg = new ChatMsg(msg, false, false);
+        mAdapter.add(chatmsg);
+    }
+
+    private void sendMsg () {
+        ChatMsg chatmsg = new ChatMsg(null, true, true);
+        mAdapter.add(chatmsg);
+        mimicOtherMsg();
+    }
+
+    private void mimicOtherMsg() {
+        ChatMsg chatMsg = new ChatMsg(null, false, true);
+        mAdapter.add(chatMsg);
+    }
 }
