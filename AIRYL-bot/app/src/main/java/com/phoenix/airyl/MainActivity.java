8f11b39e7e412d601266edd3dd697cbad24f1dfb
Rohit Das <rohit.das950@gmail.com>
2017-11-14 21:51:09 +0530

    MainActivity.java: Functions added and modified
    onCreate() has been changed to handle file read-write operations by
    the bot in the SD card or any available secondary storage.
    
    isSDCardAvailable() has been added to check if the SD card is mounted.
    
    copyFile() has been added to handle writing bot responses to file.
    
    mainFunction() has been added to handle and tracl human requests and
    bot responses.
    
    Signed-off-by: Rohit Das <rohit.das950@gmail.com>

--- AIRYL-bot/app/src/main/java/com/phoenix/airyl/MainActivity.java
+++ AIRYL-bot/app/src/main/java/com/phoenix/airyl/MainActivity.java
@@ -14,7 +14,22 @@ import android.widget.TextView;
 import com.phoenix.airyl.Adapter.ChatMsgAdapter;
 import com.phoenix.airyl.Pojo.ChatMsg;
 
+import org.alicebot.ab.AIMLProcessor;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditTxtMsg;
     private ImageView mImgView;
     private ChatMsgAdapter mAdapter;
+
+    public Bot bot;
+    public static Chat chat;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -52,6 +70,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        boolean a = isSDCARDAvailable(); //checking SD card availability
+        AssetManager assets = getResources().getAssets(); //receiving assets from app dir
+        File jaydir = new File(Environment.getExternalStorageDirectory().toString() + "airyl/bots/AIRYL");
+        boolean b = jaydir.mkdirs();
+        if (jaydir.exists()) {
+            try { //Reading the file
+                for (String dir : assets.list("AIRYL")) {
+                    File subdir = new File (jaydir.getPath() + "/" + dir);
+                    boolean subdir_check = subdir.mkdirs();
+                    for (String file: assets.list("AIRYL/" + dir)) {
+                        File f = new File(jaydir.getPath() + "/" + dir + "/" + file);
+                        if (f.exists()) {
+                            continue;
+                        }
+                        InputStream in = null;
+                        OutputStream out = null;
+                        in = assets.open("AIRYL/" + dir + "/" + file);
+                        out = new FileOutputStream(jaydir.getPath() + "/" + dir + "/" + file);
+                        //copy file from mem to SD card or 2ndary storage
+                        copyFile(in,out);
+                        in.close();
+                        in = null;
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //get the working dir
+        MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "airyl/";
+        System.out.println("Working Directory: " + MagicStrings.root_path);
+        AIMLProcessor.extension = new PCAIMLProcessorExtension();
+        //assign aiml files to bot for processing
+        bot = new Bot("AIRYL", MagicStrings.root_path, "chat");
+        chat = new Chat(bot);
+        String args[] = null;
+        mainFunction(args);
+    }
+
+    //checking SD card availability
+    public static boolean isSDCARDAvailable() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) ? true : false;
+    }
+
+    //copying the file
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != 1) {
+            out.write(buffer, 0 ,read);
+        }
+    }
+
+    //request and response of user and bot respectively
+    public static void mainFunction(String[] args) {
+        MagicBooleans.trace_mode = false;
+        System.out.println("trace mode = " + MagicBooleans.trace_mode);
+        Graphmaster.enableShortCuts = true;
+        Timer timer = new Timer();
+        String request = "Hello.";
+        String response = chat.multisentenceRespond(request);
+
+        System.out.println("Human: " + request);
+        System.out.println("Bot: " + response);
     }
 
     private void sendMsg (String msg) {
