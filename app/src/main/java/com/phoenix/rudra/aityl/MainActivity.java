562b27b3908c75baa40556fd446742589942faae
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-11-17 20:44:41 +0530

    MainActivity: Send and recieve message
    The Message in the EditText is sent as "Mine message" by adding it into
    the ChatMessageAdapter object and a dummy "Hello World" reply is sent
    as "Other Message" type.
    
    fixes: #7
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- app/src/main/java/com/phoenix/rudra/aityl/MainActivity.java
+++ app/src/main/java/com/phoenix/rudra/aityl/MainActivity.java
@@ -1,24 +1,93 @@
+/* Sheldon - MainActivity.java
+ *
+ * Copyright (C) 2017 Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
+ *
+ * Authors:
+ *   Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.phoenix.rudra.aityl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.phoenix.rudra.aityl.Adapter.ChatMessageAdapter;
+import com.phoenix.rudra.aityl.POJO.ChatMessage;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private Button mButtonSend;
+    private EditText mEditTextMessage;
+    private ImageView mImageView;
+    private ChatMessageAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.listview);
+        mButtonSend = (Button) findViewById(R.id.btn_send);
+        mEditTextMessage = (EditText) findViewById(R.id.et_message);
+        mImageView = (ImageView) findViewById(R.id.iv_image);
+        mAdapter = new ChatMessageAdapter(this, new ArrayList<ChatMessage>());
+        mListView.setAdapter(mAdapter);
+
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = mEditTextMessage.getText().toString();
+                sendMessage(message);
+                mEditTextMessage.setText("");
+                mListView.setSelection(mAdapter.getCount() - 1);
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void sendMessage(String message)
+    {
+        ChatMessage chatMessage = new ChatMessage(message, true, false);
+        mAdapter.add(chatMessage);
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+        // default "Hello World" Response
+        mimicOtherMessage("Hello World");
+    }
+
+    private void mimicOtherMessage(String message)
+    {
+        ChatMessage chatMessage = new ChatMessage(message, false, false);
+        mAdapter.add(chatMessage);
+    }
+
+    private void sendMessage()
+    {
+        ChatMessage chatMessage = new ChatMessage(null, true, true);
+        mAdapter.add(chatMessage);
+    }
+
+    private void mimicOtherMessage()
+    {
+        ChatMessage chatMessage = new ChatMessage(null, false, true);
+        mAdapter.add(chatMessage);
     }
 }
