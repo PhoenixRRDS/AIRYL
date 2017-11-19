1af0d4f26033b6ff79f4d73ce9f1ab6a750f116d
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2017-11-19 15:24:08 +0530

    MainActivity: Integrate chatbot with AIML
    Previously the chatbot only returned "Hello World" for every query by
    the user, which is now modified by adding the "Hari" bot which takes in
    queries from the user and generates the response using AIML.
    
    Due to new dynamically permission setting since Android 23, the app
    doesn't support asking from permission before installing the app. To
    avoid this, the targetSdkVersion is changed to 22
    
      targetSdkVersion 22
    
    fixes: #9
    
    Signed-off-by: Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>

--- app/src/main/java/com/phoenix/rudra/aityl/MainActivity.java
+++ app/src/main/java/com/phoenix/rudra/aityl/MainActivity.java
@@ -21,20 +21,41 @@
 
 package com.phoenix.rudra.aityl;
 
+import android.content.res.AssetManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.phoenix.rudra.aityl.Adapter.ChatMessageAdapter;
 import com.phoenix.rudra.aityl.POJO.ChatMessage;
 
+import org.alicebot.ab.AIMLProcessor;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+import org.alicebot.ab.Timer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Bot bot;
+    public static Chat chat;
 
     private ListView mListView;
     private Button mButtonSend;
@@ -57,11 +78,90 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String message = mEditTextMessage.getText().toString();
+
+                String response = chat.multisentenceRespond(message);
+                if (TextUtils.isEmpty(message)) {
+                    return;
+                }
+
                 sendMessage(message);
+                mimicOtherMessage(response);
                 mEditTextMessage.setText("");
                 mListView.setSelection(mAdapter.getCount() - 1);
             }
         });
+
+        // add "Hari" folder form assets to internal storage
+        // checking for SD card availability
+        boolean sdCardState = isSDCardAvailable();
+        // receiving the asset from the app directory
+        AssetManager assets = getResources().getAssets();
+        File botDir = new File(Environment.getExternalStorageDirectory().toString() + "/hari/bots/Hari");
+        boolean b = botDir.mkdirs();
+        if (botDir.exists()) {
+            // Reading the file
+            try {
+                for (String dir: assets.list("Hari")) {
+                    File subdir = new File(botDir.getPath() + "/" + dir);
+                    boolean subdir_check = subdir.mkdirs();
+                    for (String file : assets.list("Hari/" + dir)) {
+                        File _file = new File(botDir.getPath() + "/" + dir + "/" + file);
+                        if (_file.exists()) {
+                            continue;
+                        }
+
+                        InputStream inputStream = null;
+                        OutputStream outputStream = null;
+                        inputStream = assets.open("Hari/" + dir + "/" + file);
+                        outputStream = new FileOutputStream(botDir.getPath() + "/" + dir + "/" + file);
+                        // copy file from assets to SD card
+                        copyFile(inputStream, outputStream);
+                        inputStream.close();
+                        inputStream = null;
+                        outputStream.flush();
+                        outputStream.close();
+                        outputStream = null;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // get the working dir
+        MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "/hari";
+        System.out.println("Working Directory = " + MagicStrings.root_path);
+        AIMLProcessor.extension = new PCAIMLProcessorExtension();
+        //Assign the AIML files to bot for processing
+        bot = new Bot("Hari", MagicStrings.root_path, "chat");
+        chat = new Chat(bot);
+        String[] args = null;
+        mainFunction(args);
+    }
+
+    public static boolean isSDCardAvailable()
+    {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+    private void copyFile(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, read);
+        }
+    }
+
+    public static void mainFunction(String[] args)
+    {
+        MagicBooleans.trace_mode = false;
+        System.out.println("trace mode = " + MagicBooleans.trace_mode);
+        Graphmaster.enableShortCuts = true;
+        Timer timer = new Timer();
+        String request = "Hello.";
+        String response = chat.multisentenceRespond(request);
+
+        System.out.println("Human: "+request);
+        System.out.println("Robot: " + response);
     }
 
     private void sendMessage(String message)
